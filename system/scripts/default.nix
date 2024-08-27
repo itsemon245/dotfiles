cf8c8d093734fb67fd525e272a10175c1b9042b5
Mojahidul Islam <itsemon245@gmail.com>
2024-08-28 00:40:05 +0600

    added netspeed go script

--- system/scripts/default.nix
+++ system/scripts/default.nix
@@ -1,12 +1,12 @@
 {config, pkgs, ...}:
 let
   t = import ./t.nix {inherit pkgs; };
-  speedmeter = import ./speedmeter.nix {inherit pkgs; };
+  netspeed = import ./netspeed.nix {inherit pkgs; };
   usages = import ./usages.nix {inherit pkgs; };
 in {
     environment.systemPackages = [
         t 
-        speedmeter
+        netspeed
         usages
         pkgs.bat
     ];
