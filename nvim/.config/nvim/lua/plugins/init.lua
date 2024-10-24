d4c28aba4705e6b36ffc8b399591ec1d094f5ef9
Mojahidul Islam <itsemon245@gmail.com>
2024-10-24 23:56:34 +0600

    organized nvim config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -8,4 +8,37 @@ return {
 			local notify = require("nvim-autopairs")
 		end,
 	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"nathom/filetype.nvim",
+		opts = {
+			overrides = {
+				complex = {
+					["*.blade.php"] = "blade",
+				},
+			}
+		}
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				theme = "catppuccin",
+			},
+		},
+	},
+	{
+		"mskelton/termicons.nvim",
+		dependecies = { "nvim-tree/nvim-web-devicons" },
+		build = false,
+		opts = {},
+	}
 }
