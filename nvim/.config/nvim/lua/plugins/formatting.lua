5ea05892783c519b46e93b27c37451bcdcfc321f
Mojahidul Islam <itsemon245@gmail.com>
2024-10-27 01:46:00 +0600

    replaced null-ls with nvim-lint and added conform

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,69 @@
+-- Linter and Formatter
+local event = {
+  "BufReadPre",
+  "BufNewFile"
+}
+return {
+  -- Linter
+  {
+    'mfussenegger/nvim-lint',
+    event = event,
+    opts = {
+      linters_by_ft = {
+        lua = { 'luacheck' },
+        php = { 'phpcs' },
+      }
+    },
+    config = function()
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end
+  },
+  -- Formatter
+  {
+    'stevearc/conform.nvim',
+    event = event,
+    opts = {
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        blade = { 'blade-formatter' },
+        nix = { 'nixpkgs-fmt' },
+        php = { 'pint' }
+      }
+    },
+    config = function()
+      local conform = require('conform')
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+          conform.format({
+            lsp_fallback = true,
+            timeout_ms = 2000,
+            async = false,
+            bufnr = args.buf
+          })
+        end,
+      })
+      vim.keymap.set(
+        { 'n', 'v' },
+        '<leader>fc',
+        function()
+          conform.format({
+            lsp_fallback = true,
+            timeout_ms = 2000,
+            async = false
+          })
+        end,
+        { desc = "[F]ormat [C]ode" }
+      )
+    end
+  }
+}
