ece92d3b950edd37aa5aa46463f586b96f585f9f
Mojahidul Islam <itsemon245@gmail.com>
2024-10-26 23:33:17 +0600

    added: coc.nvim

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,21 +1,30 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		local b = null_ls.builtins
-		local f = b.formatting
-		local d = b.diagnostics
-		-- local c = b.completion
-		null_ls.setup({
-			sources = {
-				f.stylua,
-				f.pint,
-				f.prettier,
-				f.blade_formatter,
-			},
-		})
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
+    require("mason-null-ls").setup({
+      ensure_installed = { "stylua", "blade_formatter", "pint", "perttier" },
+      automatic_installation = true,
+    })
+    local null_ls = require("null-ls")
+    local b = null_ls.builtins
+    local f = b.formatting
+    local d = b.diagnostics
+    -- local c = b.completion
+    null_ls.setup({
+      sources = {
+        f.stylua,
+        f.pint,
+        f.prettier,
+        f.blade_formatter,
+      },
+    })
 
-		-- Set Keymap for formatting
-		vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "[Format] [C]urrent File", })
-	end,
+    -- Set Keymap for formatting
+    vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "[Format] [C]urrent File" })
+  end,
 }
