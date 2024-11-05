3060bcf2c761ebb72bbc3e1e5a3fd525adc1c125
Mojahidul Islam <itsemon245@gmail.com>
2024-11-05 19:38:23 +0600

    added:vscode from unstable + fix:black screen in screen sharing

--- system/gaming/default.nix
+++ system/gaming/default.nix
@@ -1,4 +1,4 @@
-{ config,pkgs, lib, ...}: 
+{ config,pkgs, lib, ...}:
 {
   hardware.opengl = {
     enable = true;
--- system/packages.nix
+++ system/packages.nix
@@ -4,12 +4,13 @@ let
     (builtins.fetchTarball {
       url = "https://github.com/nixos/nixpkgs/tarball/41dea55321e5a999b17033296ac05fe8a8b5a257";
       sha256 = "sha256:0wd5h8na7dlqdyvcvqlkgw84sj956yiq39jkljm0z7v7sg6dgwjs";
-    }) { inherit (pkgs) system;};
+    }) { inherit (pkgs) system; config.allowUnfree = true;};
 in
 {
   environment.systemPackages = with pkgs; [
     vim
     unstable.neovim
+    unstable.vscode
     lua
     luajitPackages.luarocks
     lua-language-server
@@ -20,7 +21,6 @@ in
     git
     gcc
     ripgrep
-    vscode
     tmux
     kitty
     pavucontrol
