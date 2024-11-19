71f739b16efeb295a3e01cb04433117a2ee1f6f9
Mojahidul Islam <itsemon245@gmail.com>
2024-11-19 23:54:16 +0600

    fixed: install script

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
 ## Getting Started
 
-### One Liner installtion
+> [!NOTE]
+> **This only works in NIXOS**
 
-```bash
-git clone git@github.com:bdemon245/dotfiles.git ~/dotfiles && cd ~/dotfiles && chmod +x ./install.sh && ./install.sh
+### One Liner installation
+```sh
+sh https://itsemon245.github.io/dotfiles/install.sh
 ```
-
-### Step by step installation
-
-- Clone the repository in the home directory
-
-```bash
-git clone git@github.com:bdemon245/dotfiles.git ~/dotfiles
-```
-- Change Directory
-
-```bash
-cd ~/dotfiles  
-```
-- Make the installation file executable
-
-```bash
-chmod +x ./install.sh
-```
-- Run the installation script
-
-```bash
-./install.sh
-```
-
--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p stow
+#!/usr/bin/env bash
 
-# Array of directory names
-list_dir(){
-    find . -maxdepth 1 -not -path '*/\.*' -type d | awk -F/ '{print $2}' | sort -h
+# Installs dotfiles
+git clone https://github.com/itsemon245/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+echo -e "Installing dotfiles ...\n"
+nix-shell -p stow coreutils gawk --pure --run 'sh <<EOF
+# Function to list directory names, excluding those in .stow-local-ignore
+list_dir() {
+    find . -maxdepth 1 -mindepth 1 -type d | sed "s|^\./||" | sort -h | grep -v -x -f ./.installignore
 }
 
-# List All directories and store them in a variable
-directories=($(list_dir))
+# List all directories and store them in a variable
+directories=\$(list_dir)
 
 # Loop through each directory and stow it
-for dir in "${directories[@]}"; do
-    echo "Stowing $dir ..."
-    stow "$dir"
+echo "\$directories" | while read -r dir; do
+    echo "Stowing \$dir ..."
+    stow "\$dir"
 done
+EOF'
+echo "Done!"
